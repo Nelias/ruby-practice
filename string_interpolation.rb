8459fe1771a31ed0de47728b61205ac8a84890bc
Nelias <michal.antropik@gmail.com>
2019-01-05 12:03:47 +0100

    string interpolation

---
+++ string_interpolation.rb
@@ -0,0 +1,13 @@
+p 5.to_s + " is a number five"
+
+name = "Zeus"
+
+age = 100
+
+p "Hello #{name}, this is our world"
+p "In 5 years I will be #{age + 5}"
+
+x = 5
+y = 14
+
+p "x and y added together make #{x + y}"
