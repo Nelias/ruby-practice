022eabe7fb1d8f7895e9c127509a542608892b82
Nelias <michal.antropik@gmail.com>
2019-01-07 21:17:01 +0100

    add assignment file

---
+++ assignment.rb
@@ -0,0 +1,34 @@
+a = 10
+a = a + 5
+a += 10
+p a
+
+puts
+
+b = 200
+b -= 50
+p b
+
+puts
+
+c = 300
+c *= 55
+p c
+
+puts
+
+d = 400
+d /= 55
+p d
+
+puts
+
+e = 500
+e %= 55
+p e
+
+puts
+
+f = 600
+f **= 55
+p f
