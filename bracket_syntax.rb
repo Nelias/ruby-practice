34c83918c4c1c8f89ced61d688ae45eafb4e34c5
Nelias <michal.antropik@gmail.com>
2019-01-08 17:29:26 +0100

    add bracket_syntax file

---
+++ bracket_syntax.rb
@@ -0,0 +1,13 @@
+story = "Once upon a time there and there lived a dragon"
+value = 33333333333333333333333331
+
+p story.length
+p story.size
+p story[2]
+p story.slice(6)
+p story.slice(-1)
+
+puts
+
+p value.size
+p value[4]
