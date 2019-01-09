309a5af0cb84199d0a74657af2c393660b1510dc
Nelias <michal.antropik@gmail.com>
2019-01-09 18:44:35 +0100

    add if_statement file

---
+++ if_statement.rb
@@ -0,0 +1,17 @@
+if 4 > 1
+  puts "4 is bigger than 1"
+end
+
+if 3 < 2
+  puts "this is false"
+end
+
+password = "1234567"
+
+if password == "1234567"
+  puts "congrats!"
+end
+
+if password != "1234567"
+  puts "your password is wrong"
+end
