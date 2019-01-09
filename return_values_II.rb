6ba06e493166e171d1729cec96f5bbc3e7f80951
Nelias <michal.antropik@gmail.com>
2019-01-09 15:25:28 +0100

    add return_values_II file

---
+++ return_values_II.rb
@@ -0,0 +1,18 @@
+def nothing
+end
+
+p nothing
+
+def return_string
+  "this is just a string"
+end
+
+p return_string.class
+
+def return_guess
+  puts "hehehe"
+end
+
+# puts returns nil
+p return_guess
+p return_guess.class
