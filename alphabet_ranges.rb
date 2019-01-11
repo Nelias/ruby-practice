d02f1b22f7581a1e674090fe3269ea050ef1db4a
Nelias <michal.antropik@gmail.com>
2019-01-11 18:23:52 +0100

    update alphabet_ranges file with size method

--- alphabet_ranges.rb
+++ alphabet_ranges.rb
@@ -1,7 +1,14 @@
 letters = "a".."z"
 letters2 = "A".."Z"
 letters3 = "A".."z"
+numbers = 1111..33333333
 
 p letters.last(42)
+puts
 p letters2.last(42)
+puts
 p letters3.last(42)
+puts
+
+puts letters3.size.class
+puts numbers.size
