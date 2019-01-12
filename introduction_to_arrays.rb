80d6358d83072f379f6e0fdaaa52c5ece5939dce
Nelias <michal.antropik@gmail.com>
2019-01-12 02:03:50 +0100

    update introduction_to_arrays file

--- introduction_to_arrays.rb
+++ introduction_to_arrays.rb
@@ -10,3 +10,13 @@ array = Array.new(1, 2)
 names = %w[Jane Doe Henry Bow]
 
 p Array.new(40, true)
+
+puts
+
+p numbers.length
+
+p numbers[-1]
+
+fruits = ["Mango", "Dragonfruit", "Banana", "Blackberry", "Strawberry", "Grapes", "Cherry", "Blueberry", "Gooseberry", "Pineapple", "Plum", "Pear", "Apple", "Lemon", "Orange", "Melon"]
+
+p fruits.length
