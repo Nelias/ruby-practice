0db2de4b74cb6f70207c76f91fe79905ba97c796
Nelias <michal.antropik@gmail.com>
2019-01-14 22:57:51 +0100

    update is_a? file

--- is_a?.rb
+++ is_a?.rb
@@ -1,1 +1,15 @@
 puts [1, 4].is_a?(Array)
+
+arr = [4, 5]
+
+if arr.is_a?(Array)
+  arr.each {
+    |e| puts e
+  }
+end
+
+num = 1
+
+p num.is_a?(Integer)
+p num.is_a?(Object)
+p num.is_a?(BasicObject)
