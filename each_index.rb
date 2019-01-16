5220e4233a5dd24386298e259d892510d9d352a5
Nelias <michal.antropik@gmail.com>
2019-01-16 21:23:51 +0100

    update each_index  file

--- each_index.rb
+++ each_index.rb
@@ -16,3 +16,19 @@ numbers.each_with_index do |num, index|
 end
 
 puts sum
+
+puts
+
+arr1 = [-1, 2, 1, 2, 5, 7, 3]
+
+def print_if(array)
+
+  array.each_with_index do |elem, index|
+    if elem < index
+      puts "#{elem} is #{index}"
+    end
+  end
+
+end
+
+print_if(arr1)
