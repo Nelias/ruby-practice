65b63db034fe4662f695fedf2eefb5d881d44fb7
Nelias <michal.antropik@gmail.com>
2019-01-16 20:53:27 +0100

    update each_index  file

--- each_index.rb
+++ each_index.rb
@@ -1,5 +1,18 @@
 colors = ["blue", "yellow", "green"]
 
-colors.each do |color, i|
+colors.each_with_index do |color, i|
   puts "#{color} number #{i}"
 end
+
+puts
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+
+sum = 0
+
+numbers.each_with_index do |num, index|
+  result = num * index
+  sum += result
+end
+
+puts sum
