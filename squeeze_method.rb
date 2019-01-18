8237bdd17e080b51c161a1380c9bb73b39a1049e
Nelias <michal.antropik@gmail.com>
2019-01-18 23:43:20 +0100

    update squeeze_method file

--- squeeze_method.rb
+++ squeeze_method.rb
@@ -1,3 +1,5 @@
 sentence = "the rugg is that onne    which is owned bby a rat"
+sentence_two = "the rugg is that onne    which is owned bby a rat"
 
-p sentence.squeeze
+p sentence.squeeze()
+p sentence_two.squeeze("g")
