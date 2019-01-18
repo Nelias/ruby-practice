d43d7781728dc2d3ee4506926b29a2505102b41e
Nelias <michal.antropik@gmail.com>
2019-01-18 21:59:11 +0100

    update split.rb file

--- split.rb
+++ split.rb
@@ -1,6 +1,13 @@
-sentence = "You are welcome to join us!"
+sentence = "You are welcome onetwothereere to join us!"
 
 p sentence.split
 p sentence.split("")
 p sentence.split("o")
 p sentence
+
+def longest_word(sentence)
+  words = sentence.split
+  p words.sort_by!(&:length)[-1]
+end
+
+longest_word(sentence)
