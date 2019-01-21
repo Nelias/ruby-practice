3da383c6ea6df74ec7d10c18bdcaa00124e100f5
Nelias <michal.antropik@gmail.com>
2019-01-21 22:42:43 +0100

    update create_hash file

--- create_hash.rb
+++ create_hash.rb
@@ -1,3 +1,8 @@
 nfl_roster = { "Tom Brady" => "New England Patriots",
 "Tony Romo" => "Dallas Cowboys",
 "Rob Gronkowski" => "New England Patriots" }
+
+nba_roster = {
+  "Cleveland Cavaliers" => ["Kevin Love", "LeBron James"],
+  "Golden State Warriors" => ["Stephen Curry"]
+}
