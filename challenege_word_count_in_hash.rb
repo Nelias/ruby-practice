a862244898f1f69b35a9d06bd85e0b7d4cd03e7e
Nelias <michal.antropik@gmail.com>
2019-01-23 21:28:27 +0100

    update challenge_word_count_in_hash file

--- challenege_word_count_in_hash.rb
+++ challenege_word_count_in_hash.rb
@@ -1,9 +1,10 @@
 sentence = "Once upon a time in a land far far away"
 
-def word_count(sentence)
+def word_count(string)
 
   count = Hash.new(0)
-  sentence.split().each do |e|
+  
+  string.split().each do |e|
     count[e] += 1
   end
 
