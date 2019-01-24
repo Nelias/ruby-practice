f708185de271d4f3c9c0e4ba8fc2ec88d5d47171
Nelias <michal.antropik@gmail.com>
2019-01-24 22:45:50 +0100

    update time_object file

--- time_object.rb
+++ time_object.rb
@@ -3,3 +3,17 @@ puts Time.new
 puts Time.now
 
 puts Time.new(2015, 5, 18, 7, 40, 55)
+
+today = Time.now
+
+p today.month
+p today.day
+p today.year
+p today.sec
+p today.min
+p today.hour
+
+puts
+
+p today.yday
+p today.wday
