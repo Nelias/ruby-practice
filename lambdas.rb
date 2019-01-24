1c45933a517674b3d55856998f2478685983f286
Nelias <michal.antropik@gmail.com>
2019-01-24 22:16:25 +0100

    update lambdas file

--- lambdas.rb
+++ lambdas.rb
@@ -6,3 +6,12 @@ p [1, 2, 3].map(&squares)
 p squares.call(22)
 
 p [1, 2, 3].map(&squares_lambda)
+
+
+def diet
+  status = lambda { return "You gave in" }
+  status.call
+  "You completed the diet!"
+end
+
+p diet
