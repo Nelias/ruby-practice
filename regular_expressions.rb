9c9aae6b39f87fcd43b6986fe45ad38073236a75
Nelias <michal.antropik@gmail.com>
2019-01-25 04:20:39 +0100

    update regular_expressions

--- regular_expressions.rb
+++ regular_expressions.rb
@@ -16,3 +16,11 @@ sales = "I bought 22 mangos, 4 bananas, 2 apples, 12 apricots, 5 peaches and 44 
 puts sales.scan(/[^aeiouAEIOU,\d\s\.]/)
 
 number_of_consonants = sales.scan(/[^aeiouAEIOU,\d\s\.]/).length
+
+puts
+
+puts "wordplay".sub("w", "sw")
+
+puts "555 555 222".gsub(" ", "-")
+
+puts "(555)-555 1234".gsub(/[-\s\(\)]/, "")
