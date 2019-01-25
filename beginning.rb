cf81c529155307d8943b6d96dd865f641cd54ff1
Nelias <michal.antropik@gmail.com>
2019-01-25 02:59:08 +0100

    add regular_expressions file

---
+++ beginning.rb
@@ -0,0 +1,7 @@
+puts "This is the beginning!"
+
+load "end.rb"
+
+puts "The file loaded properly."
+
+load "./end.rb"
---
+++ regular_expressions.rb
@@ -0,0 +1,5 @@
+puts //.class
+
+phrase = "one or two or more things to come today"
+
+puts phrase =~ /r/
