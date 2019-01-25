ed5ddc7ffd1a468e040c6b9cd79cf1bd0379c743
Nelias <michal.antropik@gmail.com>
2019-01-25 04:11:49 +0100

    update regular_expressions

--- regular_expressions.rb
+++ regular_expressions.rb
@@ -8,3 +8,11 @@ p phrase.scan(/o/)
 p phrase.scan(/[re]/)
 puts phrase.scan(/./)
 puts phrase.scan(/o.e/)
+
+puts
+
+sales = "I bought 22 mangos, 4 bananas, 2 apples, 12 apricots, 5 peaches and 44 pears."
+
+puts sales.scan(/[^aeiouAEIOU,\d\s\.]/)
+
+number_of_consonants = sales.scan(/[^aeiouAEIOU,\d\s\.]/).length
