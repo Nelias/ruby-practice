93fbecbac2076d65d55034dfaa4a1a912df883fe
Nelias <michal.antropik@gmail.com>
2019-01-25 18:29:11 +0100

    update gadget class

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,10 +1,22 @@
 class Gadget
+  def initialize
+    @username = "User #{rand(1..100)}"
+    @password = "topsecret"
+    @production_number = 123412312
+  end
 
+  def info
+    "Gadget #{@production_number} has the username #{@username}"
+  end
 end
 
 phone = Gadget.new
 tablet = Gadget.new
 laptop = Gadget.new
 
-puts phone
-puts tablet
+p phone
+p tablet.instance_variables
+
+puts
+
+p phone.info
