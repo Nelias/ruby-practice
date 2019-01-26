3967de39e92d064c6da7352b9060930ad87b5fd5
Nelias <michal.antropik@gmail.com>
2019-01-27 00:24:12 +0100

    update gadget class

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -7,11 +7,22 @@ class Gadget
   def initialize
     @username = "User #{rand(1..100)}"
     @password = "topsecret"
-    @production_number = 123412312
+    @production_number = generate_production_number
   end
 
   def info
     "Gadget #{@production_number} has the username #{@username} and is a #{self.class}"
+  end
+
+  private
+
+  def generate_production_number
+    start_digits = rand(10000..99999)
+    end_digits = rand(10000..99999)
+    alphabet = ("A".."Z")
+    middle_digits = "2019"
+    5.times { middle_digits << alphabet.sample }
+    "#{start_digits}-#{middle_digits}-#{end_digits}"
   end
 end
 
