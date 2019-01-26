16de9f37b096f24cb961297422c61aabb08659c3
Nelias <michal.antropik@gmail.com>
2019-01-26 01:56:29 +0100

    update

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,4 +1,9 @@
 class Gadget
+
+  attr_accessor :username
+  attr_reader :production_number
+  attr_writer :password
+
   def initialize
     @username = "User #{rand(1..100)}"
     @password = "topsecret"
@@ -7,14 +12,6 @@ class Gadget
 
   def info
     "Gadget #{@production_number} has the username #{@username} and is a #{self.class}"
-  end
-
-  def username
-    @username
-  end
-
-  def password=(new_password)
-    @password = new_password
   end
 end
 
@@ -32,3 +29,5 @@ p phone.info
 p phone.username
 
 p phone.password=("21212104021421")
+
+p phone.password = "abcedfg34324324"
