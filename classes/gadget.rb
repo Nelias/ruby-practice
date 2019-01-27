c5717a0229efd0c158221016730d8ab5390dd285
Nelias <michal.antropik@gmail.com>
2019-01-27 09:14:45 +0100

    add bank_account class

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,8 +1,7 @@
 class Gadget
 
-  attr_accessor :username
+  attr_accessor :username, :password
   attr_reader :production_number
-  attr_writer :password
 
   def initialize
     @username = "User #{rand(1..100)}"
@@ -14,15 +13,23 @@ class Gadget
     "Gadget #{@production_number} has the username #{@username} and is a #{self.class}"
   end
 
+  def password=(new_password)
+    @password = new_password if validate_password(new_password)
+  end
+
   private
 
   def generate_production_number
     start_digits = rand(10000..99999)
     end_digits = rand(10000..99999)
-    alphabet = ("A".."Z")
+    alphabet = ["Z", "B", "G", "A"]
     middle_digits = "2019"
     5.times { middle_digits << alphabet.sample }
     "#{start_digits}-#{middle_digits}-#{end_digits}"
+  end
+
+  def validate_password(new_password)
+    new_password.is_a?(String) && new_password.length >= 6
   end
 end
 
@@ -42,3 +49,8 @@ p phone.username
 p phone.password=("21212104021421")
 
 p phone.password = "abcedfg34324324"
+
+drone = Gadget.new
+puts phone.password
+
+phone.password = 123
