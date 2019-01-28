7ab21e54733bd2d02f7995a9485587eab1f7e035
Nelias <michal.antropik@gmail.com>
2019-01-28 21:42:40 +0100

    update monkey_patching file

--- classes/monkey_patching.rb
+++ classes/monkey_patching.rb
@@ -16,3 +16,25 @@ end
 scores = { a: 100,  b: 100, c: 23, d: 50, e: 13, f: 100, g: 44, h: 13 }
 
 p scores.identify_duplicate_values
+
+class Fixnum
+
+  def custom_times
+    i = 0
+    while i < self
+      yield(i + 1)
+      i += 1
+    end
+  end
+
+end
+
+5.custom_times { |i| puts i }
+
+class Book
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
+end
