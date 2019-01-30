bb8e6da7b4a09a9a500fa470e62deebfc6edbf4b
Nelias <michal.antropik@gmail.com>
2019-01-30 01:09:27 +0100

    add candidate file

---
+++ classes/candidate.rb
@@ -0,0 +1,13 @@
+class Candidate
+  attr_accessor :name, :age, :occupation, :hobby, :birthplace
+  def initialize(name, details)
+    @name = name
+    @age = details[:age]
+    @occupation = details[:occupation]
+    @hobby = details[:hobby]
+    @birthplace = details[:birthplace]
+  end
+end
+
+info = {age: 25, occupation: "Banker", hobby: "Fishing", birthplace: "Kentucky"}
+senator = Candidate.new("Mr. Smith", info)
